9d4dfd147025671dcac071c7e780d1118359262e
wenkrang <2934356978@qq.com>
2024-01-28 19:05:44 +0800

    加载信息添加

--- src/main/java/com/wenkrang/nian_beast/Nian_beast.java
+++ src/main/java/com/wenkrang/nian_beast/Nian_beast.java
@@ -1,5 +1,8 @@
 package com.wenkrang.nian_beast;
 
+import com.wenkrang.nian_beast.lib.SpigotConsoleColors;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Nian_beast extends JavaPlugin {
@@ -7,6 +10,15 @@ public final class Nian_beast extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        ConsoleCommandSender consoleSender = getServer().getConsoleSender();
+        consoleSender.sendMessage(SpigotConsoleColors.BLUE + "[*] " + SpigotConsoleColors.RESET + "正在加载Nian_beast, 版本 -=> 24a01a");
+        consoleSender.sendMessage("    _   ___                  __                    __ ");
+        consoleSender.sendMessage("   / | / (_)___ _____       / /_  ___  ____ ______/ /_");
+        consoleSender.sendMessage("  /  |/ / / __ `/ __ \\     / __ \\/ _ \\/ __ `/ ___/ __/");
+        consoleSender.sendMessage(" / /|  / / /_/ / / / /    / /_/ /  __/ /_/ (__  ) /_  ");
+        consoleSender.sendMessage("/_/ |_/_/\\__,_/_/ /_/____/_.___/\\___/\\__,_/____/\\__/  ");
+        consoleSender.sendMessage("                   /_____/                            ");
+
 
     }
 
