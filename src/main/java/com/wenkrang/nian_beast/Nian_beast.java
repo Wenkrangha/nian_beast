ccb071d30851b820e31de23ebad7c8cef345c973
wenkrang <2934356978@qq.com>
2024-01-28 17:00:43 +0800

    Initial commit

---
+++ src/main/java/com/wenkrang/nian_beast/Nian_beast.java
@@ -0,0 +1,17 @@
+package com.wenkrang.nian_beast;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nian_beast extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
