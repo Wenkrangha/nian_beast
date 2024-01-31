3b42780c6a83ff65894594ca23ec681f6e49a139
wenkrang <2934356978@qq.com>
2024-01-31 19:18:13 +0800

    更新了第二只年兽的生成机制

--- src/main/java/com/wenkrang/nian_beast/Entity/PlayerJoin.java
+++ src/main/java/com/wenkrang/nian_beast/Entity/PlayerJoin.java
@@ -18,14 +18,34 @@ import static com.wenkrang.nian_beast.Nian_beast.isShutdown;
 
 public class PlayerJoin implements Listener {
 
-    public static void findAirBlock (Location location1) {
+    /**
+     * 查找上方有空气方块的位置
+     *
+     * @param location1 起始位置
+     * @return 上方有空气方块的位置，如果没有则返回null
+     */
+    public static Location findAirBlock(Location location1) {
+        Location result = null;
         Location location = location1.clone();
-        ArrayList<Location> list = new ArrayList<>();
-        for (int i = location.getBlockY() + 1;i < 319;i++) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = location.getBlockY() + 1; i < 319; i++) {
             Location x = location.clone();
-            location.setY(location.getBlockY() + 1
-            );
+            x.setY(i);
+            if (x.getBlock().getBlockData().getMaterial().equals(Material.AIR)) {
+                list.add(x.getBlockY());
+            }
         }
+        if (!list.isEmpty()) {
+            int maxValue = list.get(0);
+            for (int num : list) {
+                if (num > maxValue) {
+                    maxValue = num;
+                }
+            }
+            result = location1.clone();
+            result.setY(maxValue);
+        }
+        return result;
     }
     @EventHandler
     public static void OnJoin(PlayerJoinEvent event) {
@@ -84,34 +104,46 @@ public class PlayerJoin implements Listener {
             }
         }.runTaskTimerAsynchronously(Nian_beast.getPlugin(Nian_beast.class), 0, 40);
         new BukkitRunnable() {
+            /**
+             * 重写的run方法
+             */
             @Override
             public void run() {
                 // 如果已经关闭，则取消任务
                 if (isShutdown) {
                     cancel();
                 }
+                // 如果玩家在线
                 if (event.getPlayer().isOnline()) {
+                    // 如果玩家所在的维度是"end"
                     if (event.getPlayer().getWorld().getName().equalsIgnoreCase("end")) {
-                        Random random = new Random();
-                        if (random.nextInt(100) < 30) {
-                            boolean hasblock = false;
-                            for (int i = event.getPlayer().getEyeLocation().getBlockY() + 1;i < 319;i++) {
-                                Location clone = event.getPlayer().getEyeLocation().clone();
-                                clone.setY(i);
-                                if (!clone.getBlock().getBlockData().getMaterial().equals(Material.AIR)) {
-                                    hasblock = true;
+                        // 如果玩家眼睛所处的方块高度小于320
+                        if (event.getPlayer().getEyeLocation().getBlockY() < 320) {
+                            Random random = new Random();
+                            // 随机数小于30时
+                            if (random.nextInt(100) < 30) {
+                                boolean hasblock = false;
+                                // 遍历玩家眼睛所处的方块以上1到319的方块
+                                for (int i = event.getPlayer().getEyeLocation().getBlockY() + 1; i < 319; i++) {
+                                    Location clone = event.getPlayer().getEyeLocation().clone();
+                                    clone.setY(i);
+                                    // 如果方块不是空气
+                                    if (!clone.getBlock().getBlockData().getMaterial().equals(Material.AIR)) {
+                                        hasblock = true;
+                                    }
+                                }
+                                // 如果玩家眼睛所处的方块以上没有方块
+                                if (!hasblock) {
+                                    // 在玩家眼睛所处的方块周围寻找空气方块
+                                    for (int i = 0; i < 3; i++) {
+                                        entity.getEntitytwo(findAirBlock(event.getPlayer().getEyeLocation()));
+                                    }
                                 }
                             }
-                            if (!hasblock) {
-                                for (int i =0;i < 3;i++) {
-
-                                }
-                            }
-
                         }
                     }
-
-                }else {
+                } else {
+                    // 如果玩家不在线，取消任务
                     cancel();
                 }
             }
--- src/main/java/com/wenkrang/nian_beast/command/nb.java
+++ src/main/java/com/wenkrang/nian_beast/command/nb.java
@@ -2,17 +2,27 @@ package com.wenkrang.nian_beast.command;
 
 import com.wenkrang.nian_beast.Entity.entity;
 import com.wenkrang.nian_beast.lib.SpigotConsoleColors;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
+
+import static com.wenkrang.nian_beast.Entity.PlayerJoin.findAirBlock;
+
 public class nb implements CommandExecutor {
-    public static void gethelp (CommandSender sender) {
+    /**
+     * 向传入的CommandSender对象发送帮助信息
+     *
+     * @param sender CommandSender对象，用于发送消息
+     */
+    public static void gethelp(CommandSender sender) {
         sender.sendMessage("§7[!]  §4年兽 - nian_beast §7正在运行");
         sender.sendMessage(" §4| §7help  帮助列表");
         sender.sendMessage(" §4| §7spawnLO 生成普通年兽");
-
     }
 
     @Override
@@ -37,9 +47,60 @@ public class nb implements CommandExecutor {
                 }
 
             }
+            if (strings[0].equalsIgnoreCase("spawnLT")) {
+                if (commandSender.isOp()) {
+                    if (commandSender instanceof Player) {
+                        Player player = (Player) commandSender;
+                        entity.getEntitytwo(findAirBlock(player.getEyeLocation()));
+                        commandSender.sendMessage(SpigotConsoleColors.BLUE + "[-] " + SpigotConsoleColors.RESET + "飞行年兽已生成");
+                    } else {
+                        commandSender.sendMessage(SpigotConsoleColors.DARK_RED + "[-] " + SpigotConsoleColors.RESET + "你必须在游戏内才可以使用此命令");
+                    }
+                } else {
+                    commandSender.sendMessage(SpigotConsoleColors.DARK_RED + "[-] " + SpigotConsoleColors.RESET + "阿巴阿巴");
+                }
+
+            }
+            if (strings[0].equalsIgnoreCase("testwo")) {
+                if (commandSender.isOp()) {
+                    if (commandSender instanceof Player) {
+                        Player player = (Player) commandSender;
+                        if (player.getPlayer().getWorld().getName().equalsIgnoreCase("end")) {
+                            // 如果玩家眼睛所处的方块高度小于320
+                            if (player.getPlayer().getEyeLocation().getBlockY() < 320) {
+                                Random random = new Random();
+                                // 随机数小于30时
+                                if (random.nextInt(100) < 30) {
+                                    boolean hasblock = false;
+                                    // 遍历玩家眼睛所处的方块以上1到319的方块
+                                    for (int i = player.getPlayer().getEyeLocation().getBlockY() + 1; i < 319; i++) {
+                                        Location clone = player.getPlayer().getEyeLocation().clone();
+                                        clone.setY(i);
+                                        // 如果方块不是空气
+                                        if (!clone.getBlock().getBlockData().getMaterial().equals(Material.AIR)) {
+                                            hasblock = true;
+                                        }
+                                    }
+                                    // 如果玩家眼睛所处的方块以上没有方块
+                                    if (!hasblock) {
+                                        // 在玩家眼睛所处的方块周围寻找空气方块
+                                        for (int i = 0; i < 3; i++) {
+                                            entity.getEntitytwo(findAirBlock(player.getPlayer().getEyeLocation()));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        commandSender.sendMessage(SpigotConsoleColors.BLUE + "[-] " + SpigotConsoleColors.RESET + "飞行年兽测试");
+                    } else {
+                        commandSender.sendMessage(SpigotConsoleColors.DARK_RED + "[-] " + SpigotConsoleColors.RESET + "你必须在游戏内才可以使用此命令");
+                    }
+                } else {
+                    commandSender.sendMessage(SpigotConsoleColors.DARK_RED + "[-] " + SpigotConsoleColors.RESET + "阿巴阿巴");
+                }
+
+            }
         }
-
-
 
 
         return true;
